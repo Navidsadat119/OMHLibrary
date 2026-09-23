bcfd77a7d8f719476626ed22848e51e5a1627f7f
Navid Sadat <148444164+Navidsadat119@users.noreply.github.com>
2026-09-24 00:18:05 +0430

    Delete app/Models directory

--- app/Models/Author.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
-class Author extends Model {
-    protected $fillable = ['name','slug','full_name','birth_year','death_year','location','biography','teachers','students','works'];
-    public function books(): BelongsToMany { return $this->belongsToMany(Book::class); }
-}
